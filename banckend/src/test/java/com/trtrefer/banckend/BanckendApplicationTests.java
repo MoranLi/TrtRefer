3155ada687afdf84cd9baaa191d9b16849facaca
Yukun Li <moran.li@usask.ca>
2021-01-18 20:57:05 -0600

    spring init

---
+++ banckend/src/test/java/com/trtrefer/banckend/BanckendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trtrefer.banckend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanckendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
